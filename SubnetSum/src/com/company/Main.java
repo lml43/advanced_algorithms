b292f813e24dd1b790953355d745d9cdec88bfec
manhlinh0403 <manhlinh0403@gmail.com>
2021-02-06 14:49:33 +0100

    sub set sum

---
+++ SubnetSum/src/com/company/Main.java
@@ -0,0 +1,71 @@
+package com.company;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> set = new ArrayList<Integer>();
+        ArrayList<Integer> l = new ArrayList<Integer>();
+
+        double epsilon = 0.4d;  // Input
+        int t = 4400;   // Input
+
+        set.add(935);   // Input
+        set.add(1045);  // Input
+        set.add(2090);  // Input
+        set.add(2134);  // Input
+        set.add(2200);  // Input
+
+        double delta = epsilon / (2 * set.size());
+
+        l.add(0);
+
+        for (int i = 0; i < set.size(); i++) {
+            int size = l.size();
+            for (int j = 0; j < size; j++) {
+                l.add(l.get(j) + set.get(i));
+            }
+            l.sort((o1, o2) -> o1 - o2);
+            System.out.println("Step " + (i + 1) + " (" + set.get(i) + ") :" + "\n");
+            System.out.print("Add: ");
+            PrintSet(l);
+            System.out.print("\nRemove: ");
+            RemoveElements(l,t);
+            PrintSet(l);
+            System.out.print("\nTrim: ");
+            TrimSet(l, delta);
+            PrintSet(l);
+            System.out.println("\n");
+        }
+
+
+    }
+
+    public static void TrimSet(ArrayList<Integer> l, double delta) {
+        for (int i = 0; i < l.size() - 1; i++) {
+            if ((double)(l.get(i) * (1 + delta)) >+ (double)(l.get(i + 1))) {
+                l.remove(i + 1);
+                i--;
+            }
+        }
+    }
+
+    public static void RemoveElements(ArrayList<Integer> l, int t) {
+        for (int i = 0; i < l.size(); i++) {
+            if(l.get(i) > t){
+                l.remove(i);
+                i--;
+            }
+        }
+    }
+
+
+    public static void PrintSet(ArrayList<Integer> l) {
+        for (int e : l) {
+            System.out.print(e + ", ");
+        }
+    }
+}
